d6ce27633c0d6a2cc363d243a91d20601692bdd8
Fath Eddine Bicane <fbicane@c4r9p6.1337.ma>
2025-01-04 14:37:53 +0100

    header plugin

--- init.lua
+++ init.lua
@@ -381,7 +381,6 @@ require("lazy").setup({
 	--
 	--
 	--
-	--
 	--smoth scroling animation
 	{
 		"karb94/neoscroll.nvim",
@@ -431,6 +430,31 @@ require("lazy").setup({
 		end,
 	},
 
+	--
+	--
+	--
+	--
+	--
+	--
+	--plugin for the 42 header
+	{
+		"Diogo-ss/42-header.nvim",
+		cmd = { "Stdheader" },
+		keys = { "<F1>" },
+		opts = {
+			default_map = true, -- Default mapping <F1> in normal mode.
+			auto_update = true, -- Update header when saving.
+			user = "fbicane", -- Your user.
+			mail = "marvin@42.fr", -- Your mail.
+			-- add other options.
+		},
+		config = function(_, opts)
+			require("42header").setup(opts)
+		end,
+	},
+	--
+	--
+	--
 	--
 	--
 	--
