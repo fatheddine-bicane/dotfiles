fa0cdc84502dbec12de0ac1d50ad1a15f6965b28
Fatheddine Bicane <fatheddinebicane@Fatheddines-MacBook-Air.local>
2025-02-02 15:38:34 +0100

    added the lazygit commint

--- init.lua
+++ init.lua
@@ -343,6 +343,42 @@ require("lazy").setup({
 	},
 	--
 	--
+	--
+	--
+	--
+	--lazygit pluging manager
+	--
+	{
+		"kdheepak/lazygit.nvim",
+		-- optional: for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			-- Set keymaps for opening lazygit
+			vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true, desc = "Open LazyGit" })
+
+			-- Configure lazygit window
+			vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
+			vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+			vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" } -- customize lazygit popup window border characters
+			vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+
+			-- Configure lazygit command to use terminal colors
+			vim.g.lazygit_use_custom_config_file_path = 0
+		end,
+	},
+	--
+	--
+	--
+	--
+	--
+	--
+	--
+	--
+	--
+	--
+	--
 	-- Function Signature Help
 	{
 		"ray-x/lsp_signature.nvim",
