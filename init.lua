8d20497b533f39a55bbb29bc5ebf327b73315cce
Fatheddine Bicane <fatheddinebicane@Fatheddines-MacBook-Air.local>
2025-02-02 15:13:01 +0100

    update

--- init.lua
+++ init.lua
@@ -930,7 +930,7 @@ require("lazy").setup({
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = { c = true, cpp = true, javascript = true }
 				local lsp_format_opt
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					lsp_format_opt = "never"
@@ -1171,6 +1171,7 @@ require("lazy").setup({
 				"typescript",
 				"tsx", -- For React
 				"json",
+				"python",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
