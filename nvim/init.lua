1cb1d758eac637fc2a39ad7cc7a03a5ba367da29
fatheddine-bicane <fatheddine04@gmail.com>
2025-04-23 22:43:13 +0100

    fix: tab esue in js (after if con)

--- nvim/init.lua
+++ nvim/init.lua
@@ -474,9 +474,19 @@ vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
 --
 --
 --this fix the identation on all files type ({} js problem)
+--this fix the identation on all files type ({} js problem)
 vim.cmd([[
+  " Default behavior: disable auto-indentation
   autocmd FileType * setlocal smartindent noautoindent nosmarttab
   autocmd FileType * setlocal indentexpr=
+  
+  " JavaScript-specific indentation rules
+  autocmd FileType javascript,javascriptreact,typescript,typescriptreact setlocal
+    \ smartindent
+    \ autoindent
+    \ cindent
+    \ cinkeys+=0{,0},0(,0),:,0#,!^F,o,O,e
+    \ indentkeys+=0{,0},0(,0),:,0#,!^F,o,O,e
 ]])
 --
 --
