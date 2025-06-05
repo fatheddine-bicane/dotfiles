d2af4818d0c743cbf8263926eec6cba3c79c260b
fatheddine-bicane <fatheddine04@gmail.com>
2025-06-05 16:33:09 +0100

    fix: 42_count_line pluging the cpace cl at start bug

--- nvim/lua/custom/plugins/42_count_line.lua
+++ nvim/lua/custom/plugins/42_count_line.lua
@@ -4,7 +4,7 @@ return {
 		"FtVim/ft_count_lines.nvim",
 		config = function()
 			require("ft_count_lines").setup({
-				enable_on_start = true, -- Enable the count lines feature at startup
+				enable_on_start = false, -- Enable the count lines feature at startup
 				keybinding = "<leader>cl", -- Keybinding to toggle the feature
 			})
 		end,
