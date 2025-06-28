57479f27fbf7315ff9b08b7c69e60a7205b142f9
fatheddine-bicane <fatheddine04@gmail.com>
2025-06-28 13:36:17 +0100

    fix: three pluging had two require lines

--- nvim/lua/custom/plugins/nvim_tree.lua
+++ nvim/lua/custom/plugins/nvim_tree.lua
@@ -4,27 +4,34 @@
 return {
 	{
 		"nvim-tree/nvim-tree.lua",
-		dependencies = "nvim-tree/nvim-web-devicons", -- Icon support for file types
+		dependencies = "nvim-tree/nvim-web-devicons",
 		config = function()
+			local keymap = vim.keymap
+
+			-- 🔑 Global keymaps
+			keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+			keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle on current file" })
+			keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+			keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+
+			-- 🧠 Setup once — includes renderer + on_attach
 			require("nvim-tree").setup({
 				view = {
-					width = 35, -- Width of the file explorer
-					relativenumber = true, -- Show relative line numbers
+					width = 35,
+					relativenumber = true,
 				},
 				renderer = {
-					indent_markers = {
-						enable = true, -- Enable folder indent markers
-					},
+					indent_markers = { enable = true },
 					icons = {
 						show = {
-							file = true, -- Show icons for files
-							folder = true, -- Show icons for folders
-							folder_arrow = true, -- Show arrows for open/closed folders
-							git = true, -- Show git icons (like changes)
+							file = true,
+							folder = true,
+							folder_arrow = true,
+							git = true,
 						},
 						glyphs = {
-							default = "", -- Default icon for unknown file types
-							symlink = "", -- Symlink icon
+							default = "",
+							symlink = "",
 							git = {
 								unstaged = "✗",
 								staged = "✓",
@@ -35,43 +42,21 @@ return {
 								ignored = "◌",
 							},
 							folder = {
-								arrow_closed = "", -- Closed folder icon
-								arrow_open = "", -- Open folder icon
-								default = "", -- Default folder icon
-								open = "", -- Open folder icon
-								empty = "", -- Empty folder icon
-								empty_open = "", -- Open empty folder icon
-								symlink = "", -- Symlinked folder icon
+								arrow_closed = "",
+								arrow_open = "",
+								default = "",
+								open = "",
+								empty = "",
+								empty_open = "",
+								symlink = "",
 							},
 						},
 					},
 				},
-			})
-
-			-- Keymaps
-			local keymap = vim.keymap -- for conciseness
-
-			keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-			keymap.set(
-				"n",
-				"<leader>ef",
-				"<cmd>NvimTreeFindFileToggle<CR>",
-				{ desc = "Toggle file explorer on current file" }
-			) -- toggle file explorer on current file
-			keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-			keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-			--
-			--
-			--
-			-- tmux
-			require("nvim-tree").setup({
 				on_attach = function(bufnr)
 					local api = require("nvim-tree.api")
-
-					-- Load default mappings first
 					api.config.mappings.default_on_attach(bufnr)
 
-					-- Helper function for keymap options
 					local function opts(desc)
 						return {
 							desc = "nvim-tree: " .. desc,
@@ -82,16 +67,113 @@ return {
 						}
 					end
 
-					-- Override with tmux navigation
 					vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", opts("Tmux Navigate Left"))
 					vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", opts("Tmux Navigate Right"))
 					vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", opts("Tmux Navigate Down"))
 					vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", opts("Tmux Navigate Up"))
 				end,
 			})
-			--
-			--
-			--
 		end,
 	},
+
+	-- {
+	-- 	"nvim-tree/nvim-tree.lua",
+	-- 	dependencies = "nvim-tree/nvim-web-devicons", -- Icon support for file types
+	-- 	config = function()
+	-- 		require("nvim-tree").setup({
+	-- 			view = {
+	-- 				width = 35, -- Width of the file explorer
+	-- 				relativenumber = true, -- Show relative line numbers
+	-- 			},
+	-- 			renderer = {
+	-- 				indent_markers = {
+	-- 					enable = true, -- Enable folder indent markers
+	-- 				},
+	-- 				icons = {
+	-- 					show = {
+	-- 						file = true, -- Show icons for files
+	-- 						folder = true, -- Show icons for folders
+	-- 						folder_arrow = true, -- Show arrows for open/closed folders
+	-- 						git = true, -- Show git icons (like changes)
+	-- 					},
+	-- 					glyphs = {
+	-- 						default = "", -- Default icon for unknown file types
+	-- 						symlink = "", -- Symlink icon
+	-- 						git = {
+	-- 							unstaged = "✗",
+	-- 							staged = "✓",
+	-- 							unmerged = "",
+	-- 							renamed = "➜",
+	-- 							untracked = "★",
+	-- 							deleted = "",
+	-- 							ignored = "◌",
+	-- 						},
+	-- 						folder = {
+	-- 							arrow_closed = "", -- Closed folder icon
+	-- 							arrow_open = "", -- Open folder icon
+	-- 							default = "", -- Default folder icon
+	-- 							open = "", -- Open folder icon
+	-- 							empty = "", -- Empty folder icon
+	-- 							empty_open = "", -- Open empty folder icon
+	-- 							symlink = "", -- Symlinked folder icon
+	-- 						},
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 		})
+	--
+	-- 		-- Keymaps
+	-- 		local keymap = vim.keymap -- for conciseness
+	--
+	-- 		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+	-- 		keymap.set(
+	-- 			"n",
+	-- 			"<leader>ef",
+	-- 			"<cmd>NvimTreeFindFileToggle<CR>",
+	-- 			{ desc = "Toggle file explorer on current file" }
+	-- 		) -- toggle file explorer on current file
+	-- 		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+	-- 		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+	-- 		--
+	-- 		--
+	-- 		--
+	-- 		-- tmux
+	-- 		require("nvim-tree").setup({
+	-- 			on_attach = function(bufnr)
+	-- 				local api = require("nvim-tree.api")
+	--
+	-- 				-- Load default mappings first
+	-- 				api.config.mappings.default_on_attach(bufnr)
+	--
+	-- 				-- Helper function for keymap options
+	-- 				local function opts(desc)
+	-- 					return {
+	-- 						desc = "nvim-tree: " .. desc,
+	-- 						buffer = bufnr,
+	-- 						noremap = true,
+	-- 						silent = true,
+	-- 						nowait = true,
+	-- 					}
+	-- 				end
+	--
+	-- 				-- Override with tmux navigation
+	-- 				vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", opts("Tmux Navigate Left"))
+	-- 				vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", opts("Tmux Navigate Right"))
+	-- 				vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", opts("Tmux Navigate Down"))
+	-- 				vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", opts("Tmux Navigate Up"))
+	-- 			end,
+	-- 		})
+	-- 		--
+	-- 		--
+	-- 		vim.api.nvim_set_hl(0, "NvimTreeGitDirty", { fg = "#ff8800" }) -- unstaged
+	-- 		vim.api.nvim_set_hl(0, "NvimTreeGitStaged", { fg = "#00ff00" }) -- staged
+	-- 		vim.api.nvim_set_hl(0, "NvimTreeGitMerge", { fg = "#ff00ff" }) -- unmerged
+	-- 		vim.api.nvim_set_hl(0, "NvimTreeGitRenamed", { fg = "#00ffff" }) -- renamed
+	-- 		vim.api.nvim_set_hl(0, "NvimTreeGitNew", { fg = "#ffff00" }) -- untracked
+	-- 		vim.api.nvim_set_hl(0, "NvimTreeGitDeleted", { fg = "#ff0000" }) -- deleted
+	-- 		vim.api.nvim_set_hl(0, "NvimTreeGitIgnored", { fg = "#aaaaaa" }) -- ignored
+	--
+	-- 		--
+	-- 	end,
+	-- },
 }
