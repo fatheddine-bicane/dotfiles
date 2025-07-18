b3fa9669e185201a67a0853c3aa4f0ffb9f2fff6
fatheddine-bicane <fatheddine04@gmail.com>
2025-07-18 10:59:57 +0100

    fix: comment the dap debugger plugin

--- nvim/lua/custom/plugins/java_lsp.lua
+++ nvim/lua/custom/plugins/java_lsp.lua
@@ -1,4 +1,5 @@
 -- NOTE: Everything necessary for java development will be installed via this plugin
+-- the github link "https://github.com/nvim-java/nvim-java?tab=readme-ov-file"
 return {
 	"nvim-java/nvim-java",
 }
