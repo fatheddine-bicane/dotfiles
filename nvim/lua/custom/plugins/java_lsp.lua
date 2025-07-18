ef85d1ed82282b744aa7c2095b7643f15b372e2c
fatheddine-bicane <fatheddine04@gmail.com>
2025-07-18 11:40:27 +0100

    feat: instructions to install the jdtls lsp (java)

--- nvim/lua/custom/plugins/java_lsp.lua
+++ nvim/lua/custom/plugins/java_lsp.lua
@@ -1,5 +1,10 @@
 -- NOTE: Everything necessary for java development will be installed via this plugin
 -- the github link "https://github.com/nvim-java/nvim-java?tab=readme-ov-file"
+
+--WARNING: the java lsp is nedded
+-- run $> brew install jdtls
+-- in nvim run :Mason and install the jdtls lsp
+
 return {
 	"nvim-java/nvim-java",
 }
