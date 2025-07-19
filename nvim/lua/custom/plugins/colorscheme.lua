8f06bdaf52f50e1c0d659e6be6800f0ff1ed3494
fatheddine-bicane <fatheddine04@gmail.com>
2025-07-19 12:48:02 +0100

    fix: fix tree gitsigns color

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -4,37 +4,62 @@ return {
 	{
 		"rebelot/kanagawa.nvim",
 		config = function()
+			--
+			--
 			-- Apply the colorscheme first
 			vim.cmd.colorscheme("kanagawa-dragon")
-
+			--
+			--
+			--
+			--
 			-- Set pure black background
 			vim.cmd("highlight Normal guibg=#000000")
-
+			--
+			--
+			--
+			--
 			-- Change the line number color
-			vim.api.nvim_set_hl(0, "LineNr", {
-				fg = "#d1cda8", -- Light beige color for line numbers
-				-- bg = "#000000", -- Pure black background
-			})
-
+			-- vim.api.nvim_set_hl(0, "LineNr", {
+			-- 	fg = "#d1cda8", -- Light beige color for line numbers
+			-- 	-- bg = "#000000", -- Pure black background
+			-- 	bg = "#282727",
+			-- })
+			--
+			--
+			--
+			--
 			-- Change NvimTree folder icon color
 			vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", {
 				fg = "#d1cda8", -- Light beige color for folder icons
 			})
-
 			--
 			--
 			--
 			--
-
+			-- -- change the tree git sign colors
+			vim.api.nvim_set_hl(0, "NvimTreeGitDirtyIcon", { fg = "#d4a76b", bg = "none" })
+			vim.api.nvim_set_hl(0, "NvimTreeGitNewIcon", { fg = "#7c936e", bg = "none" })
+			vim.api.nvim_set_hl(0, "NvimTreeGitDeletedIcon", { fg = "#b44a48", bg = "none" })
+			vim.api.nvim_set_hl(0, "NvimTreeGitRenamedIcon", { fg = "#b97871", bg = "none" })
+			vim.api.nvim_set_hl(0, "NvimTreeGitIgnoredIcon", { fg = "#757c74", bg = "none" })
+			--
+			--
+			--
+			--
 			-- Make the sign column background transparent (match Normal)
 			vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-
-			-- And also fix the sign itself (e.g., DiagnosticSignError or custom)
+			--
+			--
+			--
+			--
+			-- Make the sign column background transparent (match Normal)
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+			-- DiagnosticSignError signs color
 			vim.api.nvim_set_hl(0, "DiagnosticSignError", { bg = "none", fg = "#ff0000" }) -- or your icon color
 			vim.api.nvim_set_hl(0, "DiagnosticSignWarn", { bg = "none", fg = "#ffaa00" })
 			vim.api.nvim_set_hl(0, "DiagnosticSignInfo", { bg = "none", fg = "#00ffff" })
 			vim.api.nvim_set_hl(0, "DiagnosticSignHint", { bg = "none", fg = "#8888ff" })
-			-- fix git sign background
+			-- git signs collor in text editor
 			vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#76946a", bg = "none" }) -- green
 			vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#dca561", bg = "none" }) -- cyan
 			vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#c34043", bg = "none" }) -- red
--- nvim/lua/custom/plugins/nvim_tree.lua
+++ nvim/lua/custom/plugins/nvim_tree.lua
@@ -8,6 +8,23 @@ return {
 		config = function()
 			local keymap = vim.keymap
 
+			local function git_aware_rename(node)
+				local old_path = node.absolute_path
+				local old_name = node.name
+				local input = vim.fn.input("Rename to: ", old_name)
+				if input == "" or input == old_name then
+					return
+				end
+				local new_path = node.parent.absolute_path .. "/" .. input
+				local is_git = vim.fn.systemlist("git rev-parse --is-inside-work-tree")[1] == "true"
+				if is_git then
+					vim.cmd(string.format('silent !git mv "%s" "%s"', old_path, new_path))
+				else
+					vim.loop.fs_rename(old_path, new_path)
+				end
+				require("nvim-tree.api").tree.reload()
+			end
+
 			-- 🔑 Global keymaps
 			keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 			keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle on current file" })
@@ -37,7 +54,9 @@ return {
 								staged = "✓",
 								unmerged = "",
 								renamed = "➜",
-								untracked = "★",
+								-- untracked = "★",
+								-- untracked = "",
+								untracked = "",
 								deleted = "",
 								ignored = "◌",
 							},
@@ -80,6 +99,18 @@ return {
 							api.tree.change_root(node.absolute_path)
 						end
 					end, opts("CD into folder under cursor"))
+					--
+					--
+					--
+					-- 📝 Override `r` for Git-aware rename
+					vim.keymap.set("n", "r", function()
+						local node = api.tree.get_node_under_cursor()
+						if node then
+							git_aware_rename(node)
+						end
+					end, opts("Git-aware rename"))
+					--
+					--
 					--
 					--
 				end,
