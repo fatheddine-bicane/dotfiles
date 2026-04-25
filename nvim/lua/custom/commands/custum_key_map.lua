57b5024ad9a9eff0b3f4129ea1d63af8d285811a
fatheddine-bicane <fatheddine04@gmail.com>
2026-04-25 21:17:39 +0100

    fix (nvim): enter normal mode key changed to 'OO' insted of 'oo'

--- nvim/lua/custom/commands/custum_key_map.lua
+++ nvim/lua/custom/commands/custum_key_map.lua
@@ -4,8 +4,8 @@
 --
 -- this to map oo to act like escape key (faster way to go to normal mod)
 -- vim.api.nvim_set_keymap("n", "oo", "<Esc>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "oo", "<Esc>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "oo", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "OO", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "OO", "<Esc>", { noremap = true, silent = true })
 --
 --this will make shift l and h move by words
 vim.api.nvim_set_keymap("n", "L", "w", { noremap = true, silent = true })
